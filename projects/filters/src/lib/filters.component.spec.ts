7a978a96f6b21b404ec5e489131438349f75788b
Andrey Artemov <Andrey.Artemov@infowatch.com>
2022-01-13 22:55:07 +0300

    add new file

--- projects/filters/src/lib/filters.component.spec.ts
+++ projects/filters/src/lib/filters.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FiltersComponent } from './filters.component';
 
-describe('FiltersComponent', () => {
+describe('FiltersComponent!', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
 
