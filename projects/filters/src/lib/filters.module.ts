c43690143cf3e2fb05a6d9feeb6ee7fa95c6067d
Andrey Artemov <Andrey.Artemov@infowatch.com>
2022-01-13 22:22:44 +0300

    first

---
+++ projects/filters/src/lib/filters.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FiltersComponent } from './filters.component';
+
+
+
+@NgModule({
+  declarations: [
+    FiltersComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    FiltersComponent
+  ]
+})
+export class FiltersModule { }
---
+++ projects/integrations/src/lib/integrations.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IntegrationsComponent } from './integrations.component';
+
+
+
+@NgModule({
+  declarations: [
+    IntegrationsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    IntegrationsComponent
+  ]
+})
+export class IntegrationsModule { }
